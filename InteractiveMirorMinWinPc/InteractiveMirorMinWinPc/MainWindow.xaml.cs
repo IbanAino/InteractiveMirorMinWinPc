e85024fca756af5be69d390c38721aac8b13e6ba
IbanAino <iban@ainolabs.com>
2017-06-19 12:06:07 +0200

    Afficher l'heure et la date.

--- InteractiveMirorMinWinPc/InteractiveMirorMinWinPc/MainWindow.xaml.cs
+++ InteractiveMirorMinWinPc/InteractiveMirorMinWinPc/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace InteractiveMirorMinWinPc
 {
@@ -20,9 +21,29 @@ namespace InteractiveMirorMinWinPc
     /// </summary>
     public partial class MainWindow : Window
     {
+        // ATTRIBUTS
+        // DispatcherTimer setup
+        DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+
+        // CONSTRUCTOR
         public MainWindow()
         {
             InitializeComponent();
+
+            // display the date and the time every second
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+
+        }
+
+        // DISPLAY THE TIME AND THE DATE
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            // Updating the Label which displays the current second
+            textBlockTime.Text = DateTime.Now.ToString();
+            // Forcing the CommandManager to raise the RequerySuggested event
+            CommandManager.InvalidateRequerySuggested();
         }
     }
 }
